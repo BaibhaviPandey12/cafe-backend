df670fa052d0e2d3ac714911291a1b28c3d5dba5
BaibhaviPandey12 <rheapandey2277@gmail.com>
2025-07-11 14:29:06 +0530

    Update: made changes to XYZ feature

--- server.js
+++ server.js
@@ -13,18 +13,21 @@ app.use(express.json());
 const dbuser = encodeURIComponent(process.env.DBUSER);
 const dbpass = encodeURIComponent(process.env.DBPASS);
 
-//  Corrected cluster address: from qjxhv → cozci3l
-mongoose
-  .connect(
-    `mongodb+srv://${dbuser}:${dbpass}@cluster0.cozci3l.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`
-  )
-  .then(() => {
-    app.listen(8083, () => {
-      console.log(" Server started on port 8083");
-    });
-  })
-  .catch((err) => {
-    console.error(" MongoDB connection error:", err.message);
-  });
 
+mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
+  app.listen(8083, () => {
+    console.log("Server started");
+  });
+});
+
+// mongoose
+//   .connect(
+//     `mongodb+srv://${dbuser}:${dbpass}@cluster0.cozci3l.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`
+//   )
+//   .then(() => {
+//     app.listen(8083, () => {
+//       console.log(" Server started on port 8083");
+//     });
+//   })
+ 
 app.use("/api/user", userRouter);
